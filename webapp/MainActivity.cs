a1a9ea64c0ae5957f76cba5945d2fa93ddb1ae32
johnvanderholt <jdqwertqwert@gmail.com>
2018-05-26 14:19:33 +0200

    asdf

--- webapp/MainActivity.cs
+++ webapp/MainActivity.cs
@@ -44,29 +44,31 @@ namespace webapp
             {
 
                 // If the URL is not our own custom scheme, just let the webView load the URL as usual
-                var scheme = "hybrid:";
 
-                if (!url.StartsWith(scheme))
-                    return false;
-
-                // This handler will treat everything between the protocol and "?"
-                // as the method name.  The querystring has all of the parameters.
-                var resources = url.Substring(scheme.Length).Split('?');
-                var method = resources[0];
-                var parameters = System.Web.HttpUtility.ParseQueryString(resources[1]);
-
-                if (method == "UpdateLabel")
+                if (url.StartsWith("hybrid:"))
                 {
-                    var textbox = parameters["textbox"];
+                    // This handler will treat everything between the protocol and "?"
+                    // as the method name.  The querystring has all of the parameters.
+                    var resources = url.Substring("hybrid:".Length).Split('?');
+                    var method = resources[0];
+                    var parameters = System.Web.HttpUtility.ParseQueryString(resources[1]);
 
-                    // Add some text to our string here so that we know something
-                    // happened on the native part of the round trip.
-                    var prepended = string.Format("C# says \"{0}\"", textbox);
+                    if (method == "UpdateLabel")
+                    {
+                        var textbox = parameters["textbox"];
 
-                    // Build some javascript using the C#-modified result
-                    var js = string.Format("SetLabelText('{0}');", prepended);
+                        // Add some text to our string here so that we know something
+                        // happened on the native part of the round trip.
+                        var prepended = string.Format("C# says \"{0}\"", textbox);
 
-                    webView.LoadUrl("javascript:" + js);
+                        // Build some javascript using the C#-modified result
+                        var js = string.Format("SetLabelText('{0}');", prepended);
+
+                        webView.LoadUrl("javascript:" + js);
+                    }
+                }else if (url.StartsWith("backend:"))
+                {
+                    webView.LoadUrl("javascript:counter_update();");
                 }
 
                 return true;
---
+++ webapp/obj/Debug/android/src/md5747cff79ffd42083983be48a6d75ba86/MainActivity.java
@@ -0,0 +1,47 @@
+package md5747cff79ffd42083983be48a6d75ba86;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("webapp.MainActivity, webapp", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("webapp.MainActivity, webapp", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
