cc6d4a39a3192f71faa86ed6f7add5459fdaaa68
john <jdqwertqwert@gmail.com>
2018-05-27 01:30:25 +0200

    hopefully it fucking works now

--- vue/build/add_dist_to_proj.js
+++ vue/build/add_dist_to_proj.js
@@ -4,20 +4,27 @@ const fs = require("fs"),
 
 let base_dir = "./../../webapp/";
 
-let html = "@model webapp.Models.Model1\n" + fs.readFileSync("./../index.html", "utf8");
+let html = "@model webapp.Models.Model1\n" + fs.readFileSync("./../dist/index.html", "utf8");
 fs.writeFileSync( base_dir + "Views/RazorView.cshtml", html, "utf8");
 
 let static_dir = base_dir + "Assets/static/";
 
 if(fs.existsSync(static_dir)){
-    rm(static_dir);
+    rm(static_dir, function () {
+        rest_of_code()
+    });
+}else{
+    rest_of_code()
 }
 
-ncp.limit = 16;
+function rest_of_code(){
+    ncp.limit = 16;
 
-ncp("./../dist/static/",static_dir, function (err) {
-    if (err) {
-        return console.error(err);
-    }
-    console.log('done!');
-});
+    ncp("./../dist/static/",static_dir, function (err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('done!');
+    });
+}
+
