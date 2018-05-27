25d29893694959071fe4e3022948298fe415d018
john <jdqwertqwert@gmail.com>
2018-05-27 18:58:05 +0200

    deleted bunch of bullshit

--- webapp/obj/Debug/assets/static/scripts/utils.js
+++
@@ -1,8 +0,0 @@
-
-
-function http_get(url) {
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
